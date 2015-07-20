c0870d6f0e4155ec1bdf3a51cb9dea075ee67684
Caue Rego <caue@cregox.com>
2015-07-19 21:16:31 -0300

    minor update: mostly atom
    removed 2 useless comments on scripts.js and all the rest is thanks to
    atom

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// toggle visibility for css3 animations 
+// toggle visibility for css3 animations
 $(document).ready(function() {
 	$('header').addClass('visibility');
 	$('.carousel-iphone').addClass('visibility');
@@ -8,7 +8,7 @@ $(document).ready(function() {
 });
 
 
-//iphone carousel animation
+// iphone carousel animation
 $(window).load(function () {
 	$('header').addClass("animated fadeIn");
 	$('.carousel-iphone').addClass("animated fadeInLeft");
@@ -22,70 +22,70 @@ var scrollTop = $(window).scrollTop();
 	if (scrollTop > 200) {
 		$('.navbar-default').css('display', 'block');
 		$('.navbar-default').addClass('fixed-to-top');
-			
+
 	} else if (scrollTop == 0)   {
-	
+
 		$('.navbar-default').removeClass('fixed-to-top');
 	}
-	
-	
-//animations	
+
+
+//animations
 	$('.payoff h1').each(function(){
-			
+
 		var imagePos = $(this).offset().top;
 		var topOfWindow = $(window).scrollTop();
-			
+
 			if (imagePos < topOfWindow+650) {
 				$(this).addClass("animated fadeInLeft");
-			}		
-				
+			}
+
 	});
-	
+
 	$('.purchase button.app-store').each(function(){
-			
+
 		var imagePos = $(this).offset().top;
 		var topOfWindow = $(window).scrollTop();
-			
+
 			if (imagePos < topOfWindow+650) {
 				$(this).addClass("animated pulse");
-			}		
-				
+			}
+
 	});
-	
+
 	$('.features .col-md-4').each(function(){
-			
+
 		var imagePos = $(this).offset().top;
 		var topOfWindow = $(window).scrollTop();
-			
+
 			if (imagePos < topOfWindow+650) {
 				$(this).addClass("animated flipInX");
-			}		
-				
+			}
+
 	});
-	
+
 	$('.social .col-md-12').each(function(){
-			
+
 		var imagePos = $(this).offset().top;
 		var topOfWindow = $(window).scrollTop();
-			
+
 			if (imagePos < topOfWindow+550) {
 				$(this).addClass("animated fadeInLeft");
-			}		
-				
+			}
+
 	});
-	
+
 	$('.get-it button.app-store').each(function(){
-			
+
 		var imagePos = $(this).offset().top;
 		var topOfWindow = $(window).scrollTop();
-			
+
 			if (imagePos < topOfWindow+850) {
 				$(this).addClass("animated pulse");
-			}		
-				
+			}
+
 	});
-	
-	
+
+
 });
 
 
@@ -94,21 +94,21 @@ var scrollTop = $(window).scrollTop();
 function parallax() {
 
 		// Turn parallax scrolling off for iOS devices
-		   
+
 		    var iOS = false,
 		        p = navigator.platform;
-		
+
 		    if (p === 'iPad' || p === 'iPhone' || p === 'iPod') {
 		        iOS = true;
 		    }
-	
+
 		var scaleBg = -$(window).scrollTop() / 3;
 
         if (iOS === false) {
             $('.payoff').css('background-position-y', scaleBg - 150);
             $('.social').css('background-position-y', scaleBg + 200);
         }
-   
+
 }
 
 function navbar() {
@@ -118,48 +118,45 @@ function navbar() {
 	} else {
 	    $('#navigation').removeClass('show-nav');
 	}
-	
+
 }
 
 $(document).ready(function () {
 
 	var browserWidth = $(window).width();
-	
-	if (browserWidth > 560){ 
-	
+
+	if (browserWidth > 560){
+
 		$(window).scroll(function() {
 			parallax();
 			navbar();
 		});
-	
+
 	}
 
-});	
+});
 
 
 $(window).resize(function () {
 
 	var browserWidth = $(window).width();
-	
-	if (browserWidth > 560){ 
-	
+
+	if (browserWidth > 560){
+
 		$(window).scroll(function() {
 			parallax();
 			navbar();
 		});
-	
+
 	}
 
-});	
+});
 
 
-// iPhone Header Carousel
 $('header .carousel').carousel({
   interval: 3000
 })
 
-// iPhone Features Carousel
 $('.detail .carousel').carousel({
   interval: 4000
 })
-
