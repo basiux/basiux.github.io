c2aaf109250257c0671c04909a38feda5305d8c3
Caue Rego <caue@cregox.com>
2015-11-22 16:27:47 +0000

    convert configuration from lua
    no issues so far! :D

--- mariox.js
+++ mariox.js
@@ -1,8 +1,30 @@
 jQuery.getScript("engine.js");
 
-// Initialize Mario on first stage
+// Configuration
 
-// rather use some callback later on
-setTimeout( function pressStart () {
-  engine.keyPress(self.nes.keyboard.state1_keys.KEY_START);
-}, 2100);
+BoxRadius = 6;
+InputSize = (BoxRadius*2+1)*(BoxRadius*2+1);
+
+Inputs = InputSize+1;
+//Outputs = self.nes.keyboard.state1_keys;
+
+Population = 300;
+DeltaDisjoint = 2.0;
+DeltaWeights = 0.4;
+DeltaThreshold = 1.0;
+
+StaleSpecies = 15;
+
+MutateConnectionsChance = 0.25;
+PerturbChance = 0.90;
+CrossoverChance = 0.75;
+LinkMutationChance = 2.0;
+NodeMutationChance = 0.50;
+BiasMutationChance = 0.40;
+StepSize = 0.1;
+DisableMutationChance = 0.4;
+EnableMutationChance = 0.2;
+
+TimeoutConstant = 20;
+
+MaxNodes = 1000000;
