2e8cd794176d2ab8b47b01c3ae7a1ce1b7074ccc
Caue Rego <caue@cregox.com>
2015-11-22 16:27:10 +0000

    press start on the first screen
    since mario only have 3 lives, this should go into Ai, so it needs to
    learn how to press start as well… just a quick test case, then.

--- mariox.js
+++ mariox.js
@@ -1,1 +1,8 @@
 jQuery.getScript("engine.js");
+
+// Initialize Mario on first stage
+
+// rather use some callback later on
+setTimeout( function pressStart () {
+  engine.keyPress(self.nes.keyboard.state1_keys.KEY_START);
+}, 2100);
