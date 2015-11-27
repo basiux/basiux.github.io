1d78a779a36039e054f4eeca14ef81652ff3a768
Caue Rego <caue@cregox.com>
2015-11-27 21:26:27 +0000

    tried to make file saving work with async. it didn't.

--- mariox/files.js
+++ mariox/files.js
@@ -4,10 +4,12 @@ function writeFile (filename) { // actually using compressed localstorage instea
         poolContent.push(pool.generation);
         poolContent.push(pool.maxFitness);
         poolContent.push(pool.species);
-        var content = JSON.stringify(poolContent);
-        var compressed = LZString.compressToUTF16(content); // review - very, very slow bottleneck
-        //console.log('writing file '+ filename +' - pool size: '+ content.length +' compressed: '+ compressed.length);
-        localStorage.setItem(filename, compressed);
+        //setTimeout(function(){
+          var content = JSON.stringify(poolContent);
+          var compressed = LZString.compressToUTF16(content); // review - very, very slow bottleneck
+          console.log('writing file '+ filename +' - pool size: '+ content.length +' compressed: '+ compressed.length);
+          localStorage.setItem(filename, compressed);
+        //},0);
 }
 
 function savePool () {
@@ -19,7 +21,7 @@ function loadFile (filename) {
         var compressed = localStorage.getItem(filename);
         var content = LZString.decompressFromUTF16(compressed);
         var poolContent = jQuery.parseJSON(content);
-        //console.log('loading '+ filename +' - pool size: '+ content.length +' compressed: '+ compressed.length);
+        console.log('loading '+ filename +' - pool size: '+ content.length +' compressed: '+ compressed.length);
         pool.species = poolContent.pop();
         pool.maxFitness = poolContent.pop();
         pool.generation = poolContent.pop();
