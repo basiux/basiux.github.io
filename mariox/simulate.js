a595d4471b6de5c898213b7f7f143f2be3b64668
Caue Rego <caue@cregox.com>
2015-11-29 22:55:07 +0000

    make simulate return something, just so we know it's defined
    probably a good coding practice for all js functions always give a
    return different from “undefined”…

--- mariox/simulate.js
+++ mariox/simulate.js
@@ -2,15 +2,16 @@
 //   simulate.keyPress(self.nes.keyboard.state1_keys.KEY_START);
 var simulate = {
   keyDown: function (key) {
-    self.nes.keyboard.setKey(key, 0x41);
+    return self.nes.keyboard.setKey(key, 0x41);
   },
 
   keyUp: function (key) {
-    self.nes.keyboard.setKey(key, 0x40);
+    return self.nes.keyboard.setKey(key, 0x40);
   },
 
   keyPress: function (key) {
-    this.keyDown(key);
-    setTimeout( function () {simulate.keyUp(key)}, 500 );
+    var result = this.keyDown(key);
+    setTimeout( function () {return simulate.keyUp(key)}, 500 );
+    return result;
   }
 };
